5b02c65eec209959e9e98d0250f4755d27446cae
grufelous <grufelous@gmail.com>
2018-04-12 12:38:06 +0530

    Added splasher code

--- app/src/main/java/com/iondew/slingshot/SplashActivity.java
+++ app/src/main/java/com/iondew/slingshot/SplashActivity.java
@@ -1,13 +1,25 @@
 package com.iondew.slingshot;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SplashActivity extends AppCompatActivity {
+    private static int TIME_OUT = 5000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, TIME_OUT);
     }
 }
